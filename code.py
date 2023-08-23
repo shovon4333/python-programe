8fdef49137df4635ce2b00c8b14d7448c589f2c6
shovon333 <shovonhasan333@gmail.com>
2023-08-23 23:18:07 +0600

    second commit

--- code.py
+++ code.py
@@ -1,6 +1,16 @@
 #8. Write a Python program to reverse a tuples by using row code.
-tuplex= (10,20,30,40,50)
-rev = []
-for i in range(len(tuplex)-1, -1, -1 ):
-    rev.append(tuplex[i])
-print(tuple(rev))
+# tuplex= (10,20,30,40,50)
+# rev = []
+# for i in range(len(tuplex)-1, -1, -1 ):
+#     rev.append(tuplex[i])
+# print(tuple(rev))
+
+# 7. Write a Python program to unzip a list of tuples into individual lists
+l=[(1,2), (3,4), (8,9)]
+list1=[]
+list2=[]
+for tuplex in l:
+    list1.append(tuplex[0])
+    list2.append(tuplex[1])
+print(tuple(list1), end=" ")
+print(tuple(list2))
